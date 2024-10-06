27c4c609bb6a9d656bf1495809c3141ac32ec61a
Mr. Santosh Kumar Gupta <87138199+Sainkee@users.noreply.github.com>
2024-10-06 17:58:16 +0530

    Create mergeSort.java

---
+++ mergeSort.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class merge_Sort {
+public static void main(String[] args) {
+
+int arr[] = {0,8,0,12,15,8,0,14};
+arr = mergeSort(arr);
+System.out.println(Arrays.toString(arr));
+    
+}
+
+public static int[] mergeSort(int arr[]){
+    if(arr.length==1)return arr;
+    int mid  = arr.length/2;
+
+    int left[]  = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+    int right[] = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+    return merge(left,right);
+
+}
+
+public static int[] merge(int left[],int right []){
+    
+    int arr[] = new int [left.length+right.length];
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i<left.length && j<right.length) {
+        if(left[i]<right[j]){
+            arr[k] = left[i];
+            i++;
+        }else{
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+
+    }
+     while (i<left.length) {
+            arr[k]= left[i];
+            k++;
+            i++;
+        }
+        while(j<right.length){
+            arr[k] = right[j];
+            k++;
+            j++;
+        }
+
+    return arr;
+}
+
+
+
+    
+}
